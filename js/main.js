80b8cbe3bf7b7972625c1049143d2d5e2d92fafa
School <ivanoryshchenko@gmail.com>
2018-02-26 10:21:50 +0200

    few

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-"use strict"
+$('.hero-wrapper').waypoint(function(direction) {
+  if (direction == "down") {
+    $('.navbar').addClass('white');
+    $('#toggle').addClass('tog-move')
+  } else if (direction == "up") {
+    $('.navbar').removeClass('white');
+    $('#toggle').removeClass('tog-move')
+  }
+});
